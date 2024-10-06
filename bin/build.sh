29a6735765e5d6cf8bc72dc5a23562baf328c7ae
Md Shafiqul Islam <55863127+shafiqrbs@users.noreply.github.com>
2024-10-06 08:20:47 +0600

    Menu & routing update.

--- bin/build.sh
+++ bin/build.sh
@@ -25,3 +25,4 @@ app/console  assets:install --symlink --relative
 app/console  assetic:dump --env=prod --no-debug
 
 echo "Make directory writtable"
+chmod -R 777 app/cache app/logs web/cache web/uploads web/images
